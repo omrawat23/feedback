2d59d2356ca988f2d00c42fb7e378f5e311940e1
Om Rawat <omrawat23@gmail.com>
2024-10-24 12:55:09 +0530

    v06

--- auth.ts
+++ auth.ts
@@ -1,36 +1,35 @@
 import { AuthOptions, getServerSession } from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
 
+// Add id to Session User
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id?: string
+      email?: string
+      image?: string
+      name?: string
+    }
+  }
+}
 
 const authOptions: AuthOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    // ...add more providers here
   ],
   callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.sub
       }
-      return session
-    },
-    async jwt({ token, user}) {
-      if (user) {
-        token.id = user.id
-      }
-      return token
-    }
+    }),
   },
 }
 
-/**
- * Helper function to get the session on the server without having to import the authOptions object every single time
- * @returns The session object or null
- */
-const getSession = () => getServerSession(authOptions)
-
-export { authOptions, getSession }
+export const getSession = () => getServerSession(authOptions)
+export { authOptions }
